06ca5ee0dd9b1387628b7261e8f0fe73e895b07e
subashvelu <v.subash2410@gmail.com>
2026-09-15 16:07:34 +0000

    fix: add generateStaticParams for static export build

--- app/shop/[productId]/page.jsx
+++ app/shop/[productId]/page.jsx
@@ -1,5 +1,16 @@
-import ProductDetailClient from "../../../components/product/ProductDetailClient";
+import ProductDetailClient from '@/components/product/ProductDetailClient';
+
+export async function generateStaticParams() {
+  return [
+    { productId: '1' },
+    { productId: '2' },
+    { productId: '3' },
+    { productId: '4' },
+    { productId: 'maxera-pro' },
+    { productId: 'maxera-air' }
+  ];
+}
 
 export default function ProductDetailPage({ params }) {
-  return <ProductDetailClient slug={params.productId} />;
+  return <ProductDetailClient productId={params.productId} />;
 }
